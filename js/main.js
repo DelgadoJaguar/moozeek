b0fb895b4a7edce3e37e106f3818461c72518048
Danilo <71414290+DelgadoJaguar@users.noreply.github.com>
2021-02-18 14:45:56 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,4 +9,34 @@ var trigger = $('#hamburger'),
             menu.slideDown()
             trigger.addClass('active')
         }
-    })
+    });
+
+$('#song-lofi-btn').click(function(){
+    $("#image-info").css("background-image", "url('media/images/lo-fi.jpg')")
+    $("#title-song").text("Lo-Fi")
+    $("#desc-song").text("Lo fi sounds ")
+    });
+
+$('#song-nature-btn').click(function(){
+    $("#image-info").css("background-image", "url('media/images/nature.jpg')")
+    $("#title-song").text("Nature")
+    $("#desc-song").text("Nature sounds ")
+    });
+
+$('#song-rain-btn').click(function(){
+    $("#image-info").css("background-image", "url('media/images/rain.jpg')")
+    $("#title-song").text("Rain")
+    $("#desc-song").text("Rain so sounds ")
+    });
+
+$('#song-pink-btn').click(function(){
+    $("#image-info").css("background-image", "url('media/images/pink.jpg')")
+    $("#title-song").text("Pink")
+    $("#desc-song").text("Pink nois sounds ")
+    });
+
+$('#song-suga-btn').click(function(){
+    $("#image-info").css("background-image", "url('media/images/suga-suga.jpg')");
+    $("#title-song").text("Suga")
+    $("#desc-song").text("Suga sounds ")
+    });
